0b870a90abdea3e76d35412a2f373f88d41f2616
bambam207 <bamfordj@me.com>
2025-04-16 09:26:57 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-// Install the service worker and cache app files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('field-tools-cache').then(cache => {
@@ -7,15 +6,15 @@ self.addEventListener('install', event => {
         './style.css',
         './app.js',
         './manifest.json',
-        './icon.png',
+        './icon-192.png',
+        './icon-512.png',
         './D744E2B6-1AB3-466B-A4E8-A64711BD01F5.png'
       ]);
     })
   );
-  self.skipWaiting(); // Activate immediately
+  self.skipWaiting();
 });
 
-// Intercept requests and serve from cache first
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -24,7 +23,6 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Activate and remove old caches if necessary
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
